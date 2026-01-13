9142538eb6431fcfff739025babd972ce2f9d4ea
faridlan <faridlannulhakim@gmail.com>
2026-01-13 10:01:04 +0700

    feat: update Todo API validation and HTTP requests, add new dependencies, and set default createdAt timestamp

--- src/todo/todo.validation.ts
+++ src/todo/todo.validation.ts
@@ -1,13 +1,13 @@
-import { CreateTodoRequest, UpdateTodoRequest } from 'src/model/todo.model';
 import z, { ZodType } from 'zod';
+import { CreateTodoCommand, UpdateTodoCommand } from './model/todo.command';
 
 export class TodoValidation {
-  static readonly CREATE: ZodType<CreateTodoRequest> = z.object({
+  static readonly CREATE: ZodType<CreateTodoCommand> = z.object({
     title: z.string().min(1).max(50),
     description: z.string().min(1).max(100).optional(),
   });
 
-  static readonly UPDATE: ZodType<UpdateTodoRequest> = z.object({
+  static readonly UPDATE: ZodType<UpdateTodoCommand> = z.object({
     id: z.string().min(1),
     title: z.string().min(1).max(50).optional(),
     description: z.string().min(1).max(100).optional(),
